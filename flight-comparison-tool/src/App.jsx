4890649d2b8a9fcfdb440120128a1b61c1d7d7cc
vedapriya-vunnam <vunnamvedapriya1@gmail.com>
2026-06-17 13:28:10 +0530

    Update App.jsx

--- flight-comparison-tool/src/App.jsx
+++ flight-comparison-tool/src/App.jsx
@@ -6,6 +6,12 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import DatePrice from "./pages/DatePrice";
+import AirlineCompare from "./pages/AirlineCompare";
+import BaggagePolicy from "./pages/BaggagePolicy";
+import OTPStats from "./pages/OTPStats";
+import FilterPanel from "./pages/FilterPanel";
+import LayoverInfo from "./pages/LayoverInfo";
 import "./App.css";
 
 function App() {
@@ -19,6 +25,12 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/date-price" element={<DatePrice />} />
+<Route path="/airline-compare" element={<AirlineCompare />} />
+<Route path="/baggage-policy" element={<BaggagePolicy />} />
+<Route path="/otp-stats" element={<OTPStats />} />
+<Route path="/filter-panel" element={<FilterPanel />} />
+<Route path="/layover-info" element={<LayoverInfo />} />
       </Routes>
     </Router>
   );
